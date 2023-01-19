01215addee5c534adfe8a384d43f0d8d6fa589fa
Akash <adesigns>
2023-01-19 13:39:38 -0500

    added a buffer

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,9 @@ const App = () => {
 
     useEffect(() => {
         getVideo();
+        setTimeout(() => {
+            document.getElementById("video").style.display = "block";
+        }, 1500);
     }, []);
 
     return (
